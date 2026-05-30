cf8f17454d57748d1821f4b1fcd297f8b5a113d3
Dark-Vader39 <rustagikavyam@gmail.com>
2026-05-30 20:16:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+inp1 = ("Please ensure that all of your applications are closed before proceeding to shutdown this device (Yes/No):  ")
+if inp1 == "Yes":
+    print("You can securely shutdown your device now.")
+else:
+    print("Please close all of your applications before proceeding to shutdown this device.")
